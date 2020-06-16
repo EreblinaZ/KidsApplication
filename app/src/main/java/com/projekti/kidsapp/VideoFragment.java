f567ca84f0c12ea13752337495c8002c6422a1b2
ZanaKras <zanakrasniqi2009@live.com>
2020-06-16 02:35:23 -0700

    NumberActivity

---
+++ app/src/main/java/com/projekti/kidsapp/VideoFragment.java
@@ -0,0 +1,28 @@
+package com.projekti.kidsapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class VideoFragment extends Fragment {
+
+    
+
+
+    public VideoFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+       View view= inflater.inflate(R.layout.fragment_vdeos, container, false);
+
+       return view;
+    }
+}
