3fe2174dac951faee8c219100ce4254c55e2e52d
ZanaKras <zanakrasniqi2009@live.com>
2020-06-16 02:48:48 -0700

    NumberActivity

---
+++ app/src/main/java/com/projekti/kidsapp/fragment5.java
@@ -0,0 +1,25 @@
+package com.projekti.kidsapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class fragment5 extends Fragment {
+
+
+    public fragment5() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment5, container, false);
+    }
+}
---
+++ app/src/main/java/com/projekti/kidsapp/fragment8.java
@@ -0,0 +1,26 @@
+package com.projekti.kidsapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class fragment8 extends Fragment {
+
+
+    public fragment8() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment8, container, false);
+    }
+}
