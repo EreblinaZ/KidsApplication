a81cd6f91eeb7e6315eeaa6d2cf469422db228ad
EreblinaZ <zereblina@gmail.com>
2020-06-17 16:38:10 +0200

    Firebase Realtime Database

--- app/src/main/java/com/projekti/kidsapp/StoriesActivity.java
+++ app/src/main/java/com/projekti/kidsapp/StoriesActivity.java
@@ -1,14 +1,71 @@
 package com.projekti.kidsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StoriesActivity extends AppCompatActivity {
+
+    ListView listView;
+    DatabaseReference databaseReference;
+    List<String> title_list, answer_list;
+    ArrayAdapter<String> arrayAdapter;
+
+    Stories stories1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stories);
+
+        listView = findViewById(R.id.listView);
+
+        databaseReference= FirebaseDatabase.getInstance().getReference("kidsapp");
+        stories1 = new Stories();
+        title_list = new ArrayList<>();
+        answer_list = new ArrayList<>();
+
+        arrayAdapter = new ArrayAdapter<>(this, R.layout.item, R.id.item,title_list);
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot d:dataSnapshot.getChildren())
+                {
+                    stories1 = d.getValue(Stories.class);
+                    title_list.add(stories1.getTitle());
+                    answer_list.add(stories1.getAnswer());
+                }
+                listView.setAdapter(arrayAdapter);
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Intent intent = new Intent (StoriesActivity.this, answer.class);
+                        String p = answer_list.get(position);
+                        intent.putExtra("answer",p);
+                        startActivity(intent);
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
