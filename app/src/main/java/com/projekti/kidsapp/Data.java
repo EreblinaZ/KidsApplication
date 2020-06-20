898eea18e6982c63c1ce7340b86c3eee9452736c
ZanaKras <zanakrasniqi2009@live.com>
2020-06-19 18:04:42 -0700

    XMLParsing-AboutUsActivity

---
+++ app/src/main/java/com/projekti/kidsapp/Data.java
@@ -0,0 +1,12 @@
+package com.projekti.kidsapp;
+
+import java.util.List;
+
+public class Data {
+    public String seller, category, language, age, price, copyright;
+
+
+    public static void add(List<Data> data) {
+    }
+}
+
